280c2c485af710940a9324976735b0a53428471c
Caroline-GEORGES <carolinegeorges71@orange.fr>
2023-05-03 16:01:03 +0200

    update react v3

--- src/Controller/ReactController.php
+++ src/Controller/ReactController.php
@@ -5,17 +5,40 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ReactController extends AbstractController
-
 {
+    private $pathPublicFolder;
+
+    public function __construct(string  $publicFolderName)
+    {
+        $this->pathPublicFolder = __DIR__ . '/../../' . $publicFolderName;
+    }
+
     /**
      * @Route("/{reactRouting}", name="app_react", priority="-1", defaults={"reactRouting": null}, requirements={"reactRouting"=".+"})
      */
     public function index()
     {
-        return $this->render('react/index.html.twig');
-    }
+        // Get all js and css files generate by 'yarn build' 
+        $fullPathJsFiles = glob($this->pathPublicFolder . '/js/*.js');
+        $fullPathCssFiles = glob($this->pathPublicFolder . '/css/*.css');
 
+        // Just take the name of the files
+        $allJsFiles = [];
+        $allCssFiles = [];
+        foreach($fullPathJsFiles as $file){
+            $allJsFiles[] = basename($file);
+        }
+        foreach($fullPathCssFiles as $file){
+            $allCssFiles[] = basename($file);
+        }
+
+        return $this->render('react/index.html.twig', [
+            'jsFiles' => $allJsFiles,
+            'cssFiles' => $allCssFiles
+        ]);
+    }
 }
